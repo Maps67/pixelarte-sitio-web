6b5cd95a186cb193dddf564593243215c41b5403
Maps67 <marcoaps67@hotmail.com>
2025-10-30 23:33:38 -0600

    revert: Revertir todos los cambios del blog y CSS a la última versión estable

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://pixelartestudio.art',
-  // 'static' es el modo por defecto.
   trailingSlash: 'always',
-  // Eliminamos 'integrations' y 'adapter'
 });
