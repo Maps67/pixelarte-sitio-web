e25749859f36fd745905037c0853e575cbbe408d
Maps67 <marcoaps67@hotmail.com>
2025-11-13 15:40:44 -0600

    feat: instalar y configurar TailwindCSS correctamente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // Esta es la configuración estable y final
 export default defineConfig({
   site: 'https://pixelartestudio.art',
   trailingSlash: 'always',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
